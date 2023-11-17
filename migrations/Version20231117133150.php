febde47e29674a55b44e100931b0b757b37a4473
Theo Seuge <theoseuge@Neey.local>
2023-11-17 14:54:59 +0100

    :sparkles: Create SecurityController with login and register method

---
+++ migrations/Version20231117133150.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231117133150 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX uniq_8d93d649d17f50a6');
+        $this->addSql('ALTER TABLE "user" ALTER uuid TYPE UUID USING uuid::uuid');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE "user" ALTER uuid TYPE VARCHAR(180)');
+        $this->addSql('CREATE UNIQUE INDEX uniq_8d93d649d17f50a6 ON "user" (uuid)');
+    }
+}
